ba36388072a74efb7517b54564250480e6c1a358
Micaela Sosa <micaesosa556@gmail.com>
2021-10-20 18:00:31 -0300

    subo variables css

---
+++ app.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let app = express();
+const PORT = 3000;
+let path = require('path')
+
+//middlewares
+app.use(express.static('public'))
+
+// Routes
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, './views/index.html'))
+})
+
+//Server
+app.listen(PORT, () => console.log(`
+Servidor escuchando en el puerto ${PORT}
+http://localhost:${PORT}
+`))
