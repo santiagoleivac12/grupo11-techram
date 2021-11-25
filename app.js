77ed735f54541626396f7378f0a49c50eb1375a4
Brian <garciabrianalejandro@gmail.com>
2021-11-25 15:44:30 -0300

    subo ruta del home

--- app.js
+++ app.js
@@ -8,24 +8,25 @@ app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const indexRouter = require('./src/routes/indexRouter')
+
 // Routes
-app.get('/', function (req, res){
-    res.sendFile(path.join(__dirname, './views/index.html'))
-})
+app.use('/', indexRouter)
+
 app.get('/register', function (req, res){
-    res.sendFile(path.join(__dirname, './views/register.html'))
+    res.sendFile(path.join(__dirname, './views/register.ejs'))
 })
 app.get('/login', function (req, res){
-    res.sendFile(path.join(__dirname, './views/login.html'))
+    res.sendFile(path.join(__dirname, './views/login.ejs'))
 })
 app.get('/perfil', function (req, res){
-    res.sendFile(path.join(__dirname, './views/perfil.html'))
+    res.sendFile(path.join(__dirname, './views/perfil.ejs'))
 })
 app.get('/detalle', function (req, res){
-    res.sendFile(path.join(__dirname, './views/detalleDeProducto.html'))
+    res.sendFile(path.join(__dirname, './views/detalleDeProducto.ejs'))
 })
 app.get('/carrito', function (req, res){
-    res.sendFile(path.join(__dirname, './views/carrito.html'))
+    res.sendFile(path.join(__dirname, './views/carrito.ejs'))
 })
 
 //Server
---
+++ src/controllers/indexController.js
@@ -0,0 +1,7 @@
+const controller = {
+    index: function (req, res){
+        res.render('index')
+    }
+}
+
+module.exports = controller;
