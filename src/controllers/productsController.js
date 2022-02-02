8178d944dcd9d2ba92142bcfc9c809b1b4ce8bac
Micaela Sosa <micaesosa556@gmail.com>
2022-02-02 12:25:25 -0300

    Crud

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,11 +1,31 @@
 const fs = require('fs');
 const path = require('path')
+const db =require("../data/models")
+const Products= db.Products;
+const Categories= db.Categories;
+const Subcategories= db.Subcategories;
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json')
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+//const productsFilePath = path.join(__dirname, '../data/productsDataBase.json')
+//const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 
 const controller = {
-    detail: (req, res) =>{
+    detail:(req,res)=>{
+       Products.findOne({
+           where:{
+              id: req.params.id, 
+           },
+           include:[{association:"productImages"}]
+       })
+        .then((product=>{
+            Products.findAll({
+                where:{
+                    subcategoryId: product.subcategoryId
+                }
+            })
+        }))
+    }
+   /* detail: (req, res) =>{
         let idProduct = +req.params.id;
         let product = products.find(product => product.id === idProduct);
         res.render('products/detalleDeProducto', {
@@ -14,7 +34,7 @@ const controller = {
     },
     carrito: (req, res) =>{
         res.render('products/carrito')
-    }
+    }*/
 }
 
 module.exports = controller
