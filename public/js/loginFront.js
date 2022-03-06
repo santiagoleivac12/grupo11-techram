49c62290b1977d1f6e05552384eadc7e20bc2df7
RICARDO JUTURI <Ricardojuturi2@gmail.com>
2022-03-06 02:27:24 -0300

    fronnnttt-login

--- public/js/loginFront.js
+++ public/js/loginFront.js
@@ -2,22 +2,23 @@ function qs(element) {
     return document.querySelector(element)
 }
 
-window.addEventListener('load', ()=> {
+window.addEventListener('load', () => {
 
     let $form = qs('form'),
         $email = qs('#email-front'),
         $passFront = qs('#passFront'),
         $emailErrors = qs('#emailErrors'),
-        $passError = qs('#passError')
-    //exprecion regular para email y contraceña
+        $passError = qs('#passError'),
+        $login = qs('#login-error')
+    //exprecion regular para email y contraceñanodemon
     let regExEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
-    let regExPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+    /*  let regExPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/; */
 
     let validationsErrors = false;
 
-//email
-    $email.addEventListener('blur', function(){
-        switch (true){
+    //email
+    $email.addEventListener('blur', function () {
+        switch (true) {
             case !$email.value.trim():
                 $emailErrors.innerHTML = 'Debes completar el campo email';
                 $email.classList.add('is-invalid');
@@ -37,18 +38,18 @@ window.addEventListener('load', ()=> {
         }
     })
     //contraseña 
-    $passFront.addEventListener('blur', function(){
+    $passFront.addEventListener('blur', function () {
         switch (true) {
             case !$passFront.value.trim():
                 $passError.innerHTML = 'Debes completar el campo contraseña';
                 $passFront.classList.add('is-invalid');
                 validationsErrors = true;
                 break;
-            case !regExPass.test($passFront.value):
-                $passError.innerHTML = 'contraseña incorrecta';
-                $passFront.classList.add('is-invalid');
-                validationsErrors = true;
-                break;
+            /*  case !regExPass.test($passFront.value):
+                 $passError.innerHTML = 'contraseña incorrecta';
+                 $passFront.classList.add('is-invalid');
+                 validationsErrors = true;
+                 break; */
             default:
                 $passFront.classList.remove('is-invalid');
                 $passFront.classList.add('is-valid');
@@ -59,7 +60,17 @@ window.addEventListener('load', ()=> {
     })
     //para que el form no se envie incompleto.
     $form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        alert('tenes que completar los campos')
+     
+        
+        let error = false;
+        if(!error && !validationsErrors){
+            $form.submit()
+        }else{
+            event.preventDefault();
+            alert('tenes que completar los campos');
+        }
+
     })
+
+
 })
