dc07802953a9bc42947cf27a204b1e718913ecba
Brian <garciabrianalejandro@gmail.com>
2022-03-29 13:26:40 -0300

    le agrego estilos a la paginacion

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -49,7 +49,7 @@ let controller = {
 
         const getPagination = (page , size) => {
 
-            const limit = size ? +size : 5;
+            const limit = size ? +size : 10;
             const offset = page ? page * limit : 0;
             return{ limit, offset}
         }
